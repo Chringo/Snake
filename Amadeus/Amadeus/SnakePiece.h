5a29b3df37b0876fefa99bc135d2e1c7f5bf34c0
Christoffer Banheden <christofferb93@hotmail.com>
2017-04-23 17:01:30 +0200

    ADD essential functions to draw the snake pieces

--- Amadeus/Amadeus/SnakePiece.h
+++ Amadeus/Amadeus/SnakePiece.h
@@ -4,7 +4,22 @@
 class SnakePiece : public GridObject
 {
 public:
+	enum Piece : unsigned int
+	{
+		Body = 1,
+		Head,
+		Tail
+	};
+
 	SnakePiece();
 	~SnakePiece();
+
+	void Init(sf::Vector2i gridpos, int type);
+	void setType(Piece type);
+private:
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
+	sf::CircleShape shape;//TEST	
+	Piece m_type;
 };
 #endif // AMADEUS_MAIN_SNAKEPIECE_H_
--- Amadeus/Amadeus/Source/SnakePiece.cpp
+++ Amadeus/Amadeus/Source/SnakePiece.cpp
@@ -5,3 +5,25 @@ SnakePiece::SnakePiece()
 SnakePiece::~SnakePiece()
 {
 }
+
+void SnakePiece::Init(sf::Vector2i gridpos, int type)
+{
+	m_gridpos = gridpos;
+	m_type = Piece::Body;
+	float size = 20;
+	shape = sf::CircleShape(size / 2, 4);
+	shape.setPosition(5 + 2 + m_gridpos.x * 25.0f, 5 + 2 + m_gridpos.y * 25.0f);
+	shape.setFillColor(sf::Color::Black);
+}
+
+void SnakePiece::setType(Piece type)
+{
+	m_type = type;
+	// TODO - If we change type we need to reshape accordingly.
+}
+
+void SnakePiece::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+	//target.draw(m_sprite, states);
+	target.draw(shape, states);
+}
