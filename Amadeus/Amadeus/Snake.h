94a68b09d119a01d504449db25f43a95c4e580a3
Christoffer Banheden <christofferb93@hotmail.com>
2017-04-23 17:02:37 +0200

    ADD inclusion of SnakePiece to draw array of pieces

--- Amadeus/Amadeus/Snake.h
+++ Amadeus/Amadeus/Snake.h
@@ -1,9 +1,22 @@
 #ifndef AMADEUS_MAIN_SNAKE_H_
 #define AMADEUS_MAIN_SNAKE_H_
-class Snake
+#include <SFML\Graphics.hpp>
+#include <Amadeus\SnakePiece.h>
+class Snake : public sf::Drawable
 {
 public:
 	Snake();
 	~Snake();
+
+	void Init(sf::Vector2i gridpos);
+	void Increase(int amount = 1);
+	void Decrease(int amount = 1);
+	void Reset();
+private:
+	void Shutdown();
+	void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
+	SnakePiece *m_pieces;
+	int m_numpieces;
 };
 #endif // AMADEUS_MAIN_SNAKE_H_
--- Amadeus/Amadeus/Source/Snake.cpp
+++ Amadeus/Amadeus/Source/Snake.cpp
@@ -1,7 +1,52 @@
 #include <Amadeus\Snake.h>
 Snake::Snake()
 {
+	m_pieces = nullptr;
 }
 Snake::~Snake()
 {
+	Shutdown();
+}
+
+void Snake::Init(sf::Vector2i gridpos)
+{
+	Shutdown();
+	m_numpieces = 4;
+	m_pieces = new SnakePiece[m_numpieces];
+	sf::Vector2i gp = gridpos;
+	for (int i = 0; i < m_numpieces; i++)
+	{
+		m_pieces[i].Init(gp, SnakePiece::Body);
+		gp.x++;
+	}
+}
+
+void Snake::Increase(int amount)
+{
+}
+
+void Snake::Decrease(int amount)
+{
+}
+
+void Snake::Reset()
+{
+}
+
+void Snake::Shutdown()
+{
+	// TODO - Replace Shutdown by allocating memory for future pieces of the snake.
+	if (m_pieces)
+	{
+		delete[] m_pieces;
+		m_pieces = nullptr;
+	}
+}
+
+void Snake::draw(sf::RenderTarget & target, sf::RenderStates states) const
+{
+	for (int i = 0; i < m_numpieces; i++)
+	{
+		target.draw(m_pieces[i], states);
+	}
 }
