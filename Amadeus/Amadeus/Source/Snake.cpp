d67fd85a306d2f002493dddb55bce8122b4c803a
Christoffer Banheden <christofferb93@hotmail.com>
2017-04-25 21:53:05 +0200

    ADD initial test on increase function

--- Amadeus/Amadeus/Source/Snake.cpp
+++ Amadeus/Amadeus/Source/Snake.cpp
@@ -11,10 +11,10 @@ void Snake::Init(sf::Vector2i gridpos)
 {
 	Shutdown();
 	m_numpieces = 4;
-		m_pieces.push_back(SnakePiece());
 	sf::Vector2i gp = gridpos;
 	for (int i = 0; i < m_numpieces; i++)
 	{
+		m_pieces.push_back(SnakePiece());
 		m_pieces[i].Init(gp, SnakePiece::Body);
 		gp.x++;
 	}
@@ -22,6 +22,11 @@ void Snake::Init(sf::Vector2i gridpos)
 
 void Snake::Increase(int amount)
 {
+	//TEST
+	sf::Vector2i gp = m_pieces.back().getGridPos();
+	gp.x++;
+	m_pieces.push_back(SnakePiece());
+	m_pieces.back().Init(gp, SnakePiece::Body);
 }
 
 void Snake::Decrease(int amount)
