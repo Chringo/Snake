a841699e81e107d24b1577c393360067aa74b14a
Christoffer Banheden <christofferb93@hotmail.com>
2017-05-16 21:57:30 +0200

    ADD outcommented breaks on mem leakage

--- Amadeus/Amadeus/Source/main.cpp
+++ Amadeus/Amadeus/Source/main.cpp
@@ -1,12 +1,16 @@
 /// Use standard order for readability and to avoid hidden dependencies: 
-/// Related header, C library, C++ library, other libraries' .h, your project's .h.
+/// Related header, C library, C++ library, other libraries' .h, this project's .h.
 #include <crtdbg.h>
 #include <Amadeus\System.h>
 
 //int main(int argc, char *argv[])// Alternatively char **argv
-int main()// Empty parameter list since we're not using any commands yet
+int main()// Empty parameter list since we're not using any commands
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(9554);
+	//_CrtSetBreakAlloc(9553);
+	//_CrtSetBreakAlloc(9552);
+	//_CrtSetBreakAlloc(9609);
 	System sys;
 	sys.Init();
 	sys.Run();
